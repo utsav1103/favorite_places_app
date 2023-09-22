56ac0c37a00b333ebddf4acb4f432235fb7d88b4
utsav-baghela <utsavu858@gmail.com>
2023-09-22 11:48:42 +0530

    Adding Picked image to model

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,4 +1,4 @@
-
+import 'dart:io';
 
 import 'package:favorite_places_app/widgets/image_input.dart';
 import 'package:flutter/material.dart';
@@ -16,15 +16,16 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
     Navigator.of(context).pop();
   }
 
@@ -53,7 +54,9 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               ),
             ),
             const SizedBox(height: 10),
-            ImageInput(),
+            ImageInput(onPickImage: (image){
+              _selectedImage = image;
+            },),
 
             const SizedBox(height: 16),
             ElevatedButton.icon(
